c29e77f2cd7bd87cff6d198c54a93013a7e48155
angeaudrey <kouabenanraissa@yahoo.fr>
2023-03-15 19:25:35 +0000

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,7 +10,14 @@ import { CompteComponent } from './views/compte/compte.component';
 import { ContactComponent } from './views/contact/contact.component';
 import { MainPageComponent } from './views/main-page/main-page.component';
 import { TrocComponent } from './views/troc/troc.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import{MatStepperModule} from '@angular/material/stepper';
+import{MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import{MatIconModule} from '@angular/material/icon'
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,11 +26,20 @@ import { TrocComponent } from './views/troc/troc.component';
     CompteComponent,
     ContactComponent,
     MainPageComponent,
-    TrocComponent
+    TrocComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/troc/troc.component.ts
+++ src/app/views/troc/troc.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 @Component({
   selector: 'app-troc',
   templateUrl: './troc.component.html',
-  styleUrls: ['./troc.component.css']
+  styleUrls: ['./troc.component.css'],
+  providers:[
+    {
+    provide:STEPPER_GLOBAL_OPTIONS,
+    useValue:{showError:true}
+    },
+
+  ],
 })
 export class TrocComponent {
+
+
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['',Validators.required]
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl:['',Validators.required],
+  });
+
+  isLinear = false;
+stepper: any;
+
+  constructor(private _formBuilder: FormBuilder){
+
+  }
 
 }
